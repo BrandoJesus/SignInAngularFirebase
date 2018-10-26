da459022cff82802e2e573284ab963f9d5c0e608
Brando Jesus <brandojesus011@gmail.com>
2018-10-26 18:45:16 -0500

    Creacion de componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,13 +13,27 @@ import { AngularFireAuthModule } from '@angular/fire/auth';
 //conexion a firebase
 import { environment } from '../environments/environment';
 import { UserLoginComponent } from './users/user-login/user-login.component';
-import { UserProfileComponent } from './user-profile/user-profile.component'
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { RegisterPageComponent } from './components/register-page/register-page.component';
+import { LoginPageComponent } from './components/login-page/login-page.component';
+import { PrivadoPageComponent } from './components/privado-page/privado-page.component';
+import { NotFoundpageComponent } from './components/not-foundpage/not-foundpage.component';
+import { NotFoundPageComponent } from './components/not-found-page/not-found-page.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     UserLoginComponent,
-    UserProfileComponent
+    UserProfileComponent,
+    HomePageComponent,
+    NavbarComponent,
+    RegisterPageComponent,
+    LoginPageComponent,
+    PrivadoPageComponent,
+    NotFoundpageComponent,
+    NotFoundPageComponent
   ],
   imports: [
     BrowserModule,
